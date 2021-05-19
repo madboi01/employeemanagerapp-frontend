5128cc602cc2a0abe3da7fa92769f9de83edec81
Ankit Sarangi <66716237+madboi01@users.noreply.github.com>
2021-05-19 14:33:46 +0530

    Created and configured service class & employee interface

---
+++ src/app/employee.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiserverUrl=environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getemployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiserverUrl}/employee/all`);
+  }
+
+  public addemployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiserverUrl}/employee/add`,employee);
+  }
+
+  public updateemployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiserverUrl}/employee/all`,employee);
+  }
+
+  public findemployee(employeeId: number): Observable<Employee> {
+    return this.http.get<Employee>(`${this.apiserverUrl}/employee/find/${employeeId}`);
+  }
+
+  public deleteemployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiserverUrl}/employee/delete/${employeeId}`);
+  }
+
+}
